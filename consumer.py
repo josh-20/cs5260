e2ce01bfc492dc9708ede501c3f4295b16978a6b
Josh Hamby <bubbahamby@gmail.com>
2023-11-05 14:33:52 -0700

    working update delete for queue requests and bucket requests

--- consumer.py
+++ consumer.py
@@ -3,6 +3,7 @@ import boto3
 import time
 import logging
 import json
+import traceback
 
 class Consumer:
     def __init__(self):
@@ -14,6 +15,7 @@ class Consumer:
     def createDBItem(self, object):
         item = {
         'id': {'S': object['widgetId']},
+        'requestId': {'S': object['requestId']},
         'owner': {'S': object['owner']},
         'label': {'S': object['label']},
         'description': {'S': object['description']},
@@ -32,13 +34,83 @@ class Consumer:
         return item
         # Your createDBItem implementation here
 
-    def updateDBItem(self, object, dbname):
+    def updateDBItem(self, json_data, dbname):
         # Your updateDBItem implementation here
-        pass
+        try:
+            key = {'id': {'S': json_data['widgetId']}}
+            
+            if('label' not in json_data):
+                update_expression = 'SET requestId = :newRequestId, #owner = :newOwner, description = :newDescription, otherAttributes = :newOtherAttributes'
+                expression_attribute_values = {
+                    ':newRequestId': {'S': json_data['requestId']},
+                    ':newOwner': {'S': json_data['owner']},
+                    ':newDescription': {'S': json_data['description']},
+                    ':newOtherAttributes': {'L': []}
+                }
+                expression_attribute_names = {
+                    '#owner': 'owner',  # Define the placeholder for 'owner'
+                }
+                if 'otherAttributes' in json_data:
+                    for attribute in json_data['otherAttributes']:
+                        if 'name' in attribute and 'value' in attribute:
+                            expression_attribute_values[':newOtherAttributes']['L'].append({
+                                'M': {
+                                    'name': {'S': attribute['name']},
+                                    'value': {'S': attribute['value']}
+                                }
+                            })
+                
+                response = self.dynamoDB.update_item(TableName=dbname, Key=key, UpdateExpression=update_expression, ExpressionAttributeValues=expression_attribute_values,ExpressionAttributeNames=expression_attribute_names)
+                logging.info(f"Update item in {dbname}{response}")
+            else:
+                update_expression = 'SET requestId = :newRequestId, #owner = :newOwner, #label = :newLabel, description = :newDescription, otherAttributes = :newOtherAttributes'
+                expression_attribute_values = {
+                    ':newRequestId': {'S': json_data['requestId']},
+                    ':newOwner': {'S': json_data['owner']},
+                    ':newLabel': {'S': json_data['label']},
+                    ':newDescription': {'S': json_data['description']},
+                    ':newOtherAttributes': {'L': []}
+                }
+                expression_attribute_names = {
+                    '#owner': 'owner',  # Define the placeholder for 'owner'
+                    '#label': 'label'  # Define the placeholder for 'label'
+                }
+                if 'otherAttributes' in json_data:
+                    for attribute in json_data['otherAttributes']:
+                        if 'name' in attribute and 'value' in attribute:
+                            expression_attribute_values[':newOtherAttributes']['L'].append({
+                                'M': {
+                                    'name': {'S': attribute['name']},
+                                    'value': {'S': attribute['value']}
+                                }
+                            })
+                
+                response = self.dynamoDB.update_item(TableName=dbname, Key=key, UpdateExpression=update_expression, ExpressionAttributeValues=expression_attribute_values,ExpressionAttributeNames=expression_attribute_names)
+                logging.info(f"Update item in {dbname}{response}")
+        except Exception as e:
+            logging.error(f"Issue Updating dynamoDB Item {e}")
+            traceback.print_exc()
+ 
 
-    def deleteDBItem(self, object, dbname):
-        # Your deleteDBItem implementation here
-        pass
+    def process_DB_request(self, dbname, data):
+        try:
+            json_data = json.loads(data)
+            if(json_data['type'] == 'create'):
+                item = self.createDBItem(json_data)
+                response = self.dynamoDB.put_item(TableName=dbname,Item=item)
+                logging.info(f'Object uploaded successfully to {dbname}')
+            elif(json_data['type'] == 'delete'):
+                try:
+                    key = {'id': {'S': json_data['widgetId']}}
+                    response = self.dynamoDB.delete_item(TableName=dbname, Key=key)
+                    logging.info(f"Deleted Item from {dbname}{response}")
+                except Exception as e:
+                    logging.error(f"Issues Deleting Item from dynamoDB {e}")
+            elif(json_data['type'] == 'update'):
+                self.updateDBItem(json_data,dbname)
+                
+        except Exception as e:
+            logging.error(e)
 
     def process_request_db(self, type, source, dbname=''):
         while True:
@@ -60,14 +132,7 @@ class Consumer:
                     data = object['Body']
                     json_data = json.loads(data)
                     if data:
-                        try:
-                            json_data = json.loads(data)
-                            if(json_data['type'] == 'create'):
-                                item = self.createDBItem(json_data)
-                                response = self.dynamoDB.put_item(TableName=dbname,Item=item)
-                                logging.info(f'Object uploaded successfully to {dbname}')
-                        except Exception as e:
-                            logging.error(e)
+                        self.process_DB_request(dbname,data)
                         try:
                             response = self.queue.delete_message(QueueUrl=queue_url, ReceiptHandle=object['ReceiptHandle'])
                             logging.info(f"deleted{response}")
@@ -86,14 +151,7 @@ class Consumer:
                     # Process the object as needed
                     data = response['Body'].read().decode('utf-8')
                     if data:
-                        try:
-                            json_data = json.loads(data)
-                            if(json_data['type'] == 'create'):
-                                item = self.createDBItem(json_data)
-                                response = self.dynamoDB.put_item(TableName=dbname,Item=item)
-                                logging.info(f'Object uploaded successfully to {dbname}')
-                        except Exception as e:
-                            logging.error(e)
+                        self.process_DB_request(dbname,data)
                     try:
                         self.s3.delete_object(Bucket=source, Key=object['Key'])
                     except Exception as e:
@@ -101,13 +159,32 @@ class Consumer:
                 else:
                     time.sleep(10)
                     print("waiting")
+                    
+    def process_s3_Request(self,json_data, destination_bucket, data):
+        try:
+            if(json_data['type'] == 'create'):
+                self.s3.put_object(Bucket=destination_bucket, Key=json_data['requestId'], Body=data)
+                logging.info('Object uploaded successfully to '+ json_data['requestId'])
+            elif (json_data['type'] == 'delete'):
+                try:
+                    response = self.s3.delete_object(Bucket=destination_bucket, Key=json_data['requestId'])
+                    logging.info(f"Deleted: {response}")
+                except Exception as e:
+                    logging.error(f"Issue Deleting from S3 bucket: {e}")
+            elif(json_data['type'] == 'update'):
+                try:
+                    response = self.s3.put_object(Bucket=destination_bucket, Key=json_data['requestId'], Body=data)
+                    logging.info(f"updated Item: {response}")
+                except Exception as e:
+                    logging.error(f"Issue updating Item in s3 bucket: {e}")
+        except Exception as e:
+            logging.error(f"Issue in process_s3_Request: {e}")
 
-    def process_requests_bucket(self,type,source, destination_bucket, prefix=""):
+    def process_requests_bucket(self,type,source, destination_bucket):
         condition = True
         while condition:
             if(type == "queue"):
-                    queue_name = source
-                    response =self.queue.get_queue_url(QueueName=queue_name)
+                    response =self.queue.get_queue_url(QueueName=source)
                     queue_url = response['QueueUrl']
                     try:
                         response = self.queue.receive_message(
@@ -123,13 +200,7 @@ class Consumer:
                         data = object['Body']
                         json_data = json.loads(data)
                         if data:
-                            try:
-                                json_data = json.loads(data)
-                                if(json_data['type'] == 'create'):
-                                    self.s3.put_object(Bucket=destination_bucket, Key=json_data['requestId'], Body=data)
-                                    logging.info('Object uploaded successfully to '+ json_data['requestId'])
-                            except Exception as e:
-                                logging.error(e)
+                            self.process_s3_Request(json_data, destination_bucket, data)
                             try:
                                 response = self.queue.delete_message(QueueUrl=queue_url, ReceiptHandle=object['ReceiptHandle'])
                                 logging.info(f"deleted: {response}")
@@ -144,14 +215,9 @@ class Consumer:
 
                     # Process the object as needed
                     data = response['Body'].read().decode('utf-8')
+                    json_data = json.loads(data)
                     if data:
-                        try:
-                            json_data = json.loads(data)
-                            if(json_data['type'] == 'create'):
-                                self.s3.put_object(Bucket=destination_bucket, Key=object['Key'], Body=data)
-                                logging.info('Object uploaded successfully to '+ object['Key'])
-                        except Exception as e:
-                            logging.error(e)
+                        self.process_s3_Request(json_data, destination_bucket, data)
                     try:
                         self.s3.delete_object(Bucket=source, Key=object['Key'])
                     except Exception as e:
@@ -188,7 +254,7 @@ class Consumer:
         args = parser.parse_args()
 
         if args.command == 's3':
-            self.process_requests_bucket('s3',args.source_bucket, args.destination_bucket, args.prefix)
+            self.process_requests_bucket('s3',args.source_bucket, args.destination_bucket)
         elif args.command == 'dynamodb':
             self.process_request_db('s3',args.source_bucket, args.db_name)
         elif args.command == 'queue':
